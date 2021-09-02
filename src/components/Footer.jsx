1674ceb8895d9585f6d162062a96edcfc41ba894
LTonyan <liana.tonian@gmail.com>
2021-09-02 12:41:01 +0400

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Input } from 'antd';
-const year = new Date().getFullYear();
+
+import { useSubscription } from "use-subscription";
 
 const Footer = () => {
     const { Search } = Input;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,21 +8,25 @@ const HomePage = () => {
     const imagesArr = ['/assets/images/fake/slider1.png', '/assets/images/fake/slider2.png'];
     return (
         <div className='home-page'>
-            <div className='hhh'>
-                <div className='home-page-top'>
+            <div className='home-page-top'>
+                <div className='home-com-int'>
                     <ComicsIntroduction
                         coverName='01'
                         headerText='“Velvet Revolution”'
                         text='comics'
                         btnText='Buy now'
                         shortText='The "Velvet Revolution" comic book is about the events that took 
-                    place in Armenia a year ago, and was later characterized as "revolution" by people in Armenia. '
+                        place in Armenia a year ago, and was later characterized as "revolution" by people in Armenia. '
                         isVisible
                     />
+                </div>
+                <div className='home-images-slider'>
                     <HomePageSlider imagesArr={imagesArr} />
                 </div>
-                <ComicsDesc />
-                <div className='home-page-top hame-page-cooparate'>
+            </div>
+            <ComicsDesc />
+            <div className='home-page-top hame-page-cooparate'>
+                <div className='home-com-int'>
                     <ComicsIntroduction
                         headerText='Cooperate is'
                         text='great'
@@ -31,13 +35,15 @@ const HomePage = () => {
                     characterized as "revolution" by people in Armenia. This is not a story about politicians, or about the development of 
                     political movements in the country, or a revolution or a change of power, instead its a story about the victory of the people and peace. '
                     />
+                </div>
+                <div className='home-images-slider'>
                     <HomePageSlider imagesArr={imagesArr} />
                 </div>
-                <div className='home-page-image'>
-                    <img src='/assets/images/homePage.png' alt='home page image' />
-                </div>
-                <PageFooter />
             </div>
+            <div className='home-page-image'>
+                <img src='/assets/images/homePage.png' alt='home page image' />
+            </div>
+            <PageFooter />
         </div>
     )
 };
